cf42ebda6677323aa2a6d18054cf3789dcbc0d9d
Stefan Lindbohm <stefan@kolizz.com>
2013-05-19 20:43:03 +0200

    Nicer styling and localization for food info
    - Symbol font for illustration niceness
    - Some localization strings and helpers for nicer date output
    - Styling for form errors
    - Styling for food services on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_filter :require_authentication
 
   def index
-    @next_food_service = @current_event.food_services.upcoming.order('opens_at ASC').includes(:dishes).first
+    @upcoming_food_services = @current_event.food_services.upcoming.order('opens_at ASC').includes(:dishes).limit(2)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,38 @@ module ApplicationHelper
   def administrator?
     @current_person && @current_person.administrator?
   end
+
+  def distance_of_time_in_hours_and_minutes_to_now(time)
+    distance_in_seconds = (time.to_time.to_f - Time.now.to_f).abs
+
+    distance_in_minutes = distance_in_seconds / 60
+    hours = (distance_in_minutes / 60).floor
+    minutes = (hours > 0 ? (distance_in_minutes % 60) : distance_in_minutes).round
+
+    parts = []
+    parts << "#{hours} h" if hours > 0
+    parts << "#{minutes} min"
+
+    parts.join(' ')
+  end
+
+  def human_relatable_time(time, explicit_today = false)
+    date = time.to_date
+    case date
+    when Date.today
+      if explicit_today
+        "#{t :'date.today'} #{l time, format: :time}"
+      else
+        localize(time, format: :time)
+      end
+    when Date.yesterday
+      "#{t :'date.yesterday'} #{l time, format: :time}"
+    when Date.tomorrow
+      "#{t :'date.tomorrow'} #{l time, format: :time}"
+    when (Date.today - 6)..(Date.today + 7)
+      localize(time, format: :weekday_time)
+    else
+      localize(time, format: :short)
+    end
+  end
 end
