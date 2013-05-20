56e7b5bc7afc8725f1eb72e6984f27507a55e946
Stefan Lindbohm <stefan@kolizz.com>
2013-05-20 03:13:15 +0200

    Remove secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Crewservicer::Application.config.secret_token = 'c628baf21d4baac011ba0003a4e11d0c7bcf8fe283f042c2856773fda297a918034abb88ba9f9e9d9f081d4efeecb0d5fd529187e2919c044687d61a5e66e994'
