0306a0564c3cd42e3120e4aea8923c1951be9541
Stefan Lindbohm <stefan@kolizz.com>
2013-05-20 23:18:41 +0200

    Make unicorn.rb secret

---
+++ config/unicorn.example.rb
@@ -0,0 +1,5 @@
+listen '/path/to/app/tmp/sockets/unicorn.sock'
+worker_processes 4
+pid '/path/to/app/tmp/pids/unicorn.pid'
+stderr_path '/path/to/app/log/unicorn.log'
+stdout_path '/path/to/app/log/unicorn.log'
--- config/unicorn.rb
+++
@@ -1,5 +0,0 @@
-listen './tmp/sockets/unicorn.sock'
-worker_processes 4
-pid './tmp/pids/unicorn.pid'
-stderr_path './log/unicorn.log'
-stdout_path './log/unicorn.log'
