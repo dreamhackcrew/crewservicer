9b280111e6d30bf3c6ae934564ea47a549137be0
Stefan Lindbohm <stefan@kolizz.com>
2013-05-20 23:08:12 +0200

    Precompile admin assets

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module Crewservicer
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += [ 'admin.js', 'admin.css' ]
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
