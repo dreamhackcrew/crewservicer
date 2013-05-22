efc5abf44bcc0dad97df276c864406451d09e33e
Stefan Lindbohm <stefan@kolizz.com>
2013-05-22 23:46:35 +0200

    Handle ActiveRecord reconnect for Unicorn forking

--- config/unicorn.example.rb
+++ config/unicorn.example.rb
@@ -4,3 +4,11 @@ pid '/path/to/app/tmp/pids/unicorn.pid'
 stderr_path '/path/to/app/log/unicorn.log'
 stdout_path '/path/to/app/log/unicorn.log'
 preload_app true
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+end
