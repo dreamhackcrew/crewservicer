c10c7140527e31d726003db08b76da87c65a037e
Stefan Lindbohm <stefan@kolizz.com>
2013-06-07 02:03:37 +0200

    Show ordered t-shirts a bit nicer

--- app/controllers/admin/t_shirt_orders_controller.rb
+++ app/controllers/admin/t_shirt_orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::TShirtOrdersController < ApplicationController
   before_filter :require_administrator_privileges
 
   def index
-    @crew_t_shirt_orders = @current_event.t_shirt_orders.where(model: :crew).includes(:person)
-    @gift_t_shirt_orders = @current_event.t_shirt_orders.where(model: :gift).includes(:person)
+    @crew_t_shirt_orders = @current_event.t_shirt_orders.where(model: :crew).joins(:person).includes(:person).order('people.username ASC')
+    @gift_t_shirt_orders = @current_event.t_shirt_orders.where(model: :gift).joins(:person).includes(:person).order('people.username ASC')
   end
 end
