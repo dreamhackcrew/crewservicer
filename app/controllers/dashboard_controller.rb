851fca37b9bebe6f66f92983c0196ea76539ede8
Stefan Lindbohm <stefan@kolizz.com>
2013-06-08 16:12:24 +0200

    Nicer message when no food services are added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,12 +3,14 @@ class DashboardController < ApplicationController
 
   def index
     @upcoming_food_services = @current_event.food_services.upcoming.order('opens_at ASC').includes(:dishes).limit(2)
+    @food_services_pending = @upcoming_food_services.empty? && @current_event.food_services.empty?
     @messages = Message.published.on_site.order('sort_priority ASC, published_at DESC')
   end
 
   def food_services
     @upcoming_food_services = @current_event.food_services.upcoming.order('opens_at ASC').includes(:dishes)
     @past_food_services = @current_event.food_services.past.order('opens_at DESC').includes(:dishes)
+    @food_services_pending = @upcoming_food_services.empty? && @current_event.food_services.empty?
   end
 
   def messages
